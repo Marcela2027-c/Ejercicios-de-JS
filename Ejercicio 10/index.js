ddf29cbe3600e017eccdb4406118cec3d95079a9
Marcela2027-c <coca.ramirez2027@hilasal.superate.org>
2026-05-13 22:04:37 -0600

    Add user authentication prompt in index.js

---
+++ Ejercicio 10/index.js
@@ -0,0 +1,9 @@
+let user =prompt("Ingrese su usuario")
+let password = prompt("Ingrese su contraseña")
+ 
+ 
+if (user === "Name@" && password === "7894") {
+    console.log("Bienvenido")
+} else {
+    console.log("Credenciales incorrectas")
+}
